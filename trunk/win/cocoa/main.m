37dd48d66b3adb8cace1d09b08e9f119f7519534
google@dirkz.com <google@dirkz.com@d6335498-75d6-11de-abb5-1d1887d8974b>
2009-07-21 09:30:46 +0000

    initial skeleton

---
+++ trunk/win/cocoa/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  nethack-cocoa
+//
+//  Created by dirk on 7/21/09.
+//  Copyright Dirk Zimmermann 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
