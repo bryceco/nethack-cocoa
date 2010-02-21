2d33c8a362db4c8a58d8350d92a54c1ba3e1aeea
bryceco <bryceco@d6335498-75d6-11de-abb5-1d1887d8974b>
2010-02-21 08:50:04 +0000

    Add a few missing files

---
+++ trunk/nethack/win/cocoa/FlippedView.m
@@ -0,0 +1,18 @@
+//
+//  FlippedView.m
+//  NetHackCocoa
+//
+//  Created by Bryce on 2/17/10.
+//  Copyright 2010 Apple Inc. All rights reserved.
+//
+
+#import "FlippedView.h"
+
+
+@implementation FlippedView
+
+- (BOOL)isFlipped
+{
+	return YES;
+}
+@end
