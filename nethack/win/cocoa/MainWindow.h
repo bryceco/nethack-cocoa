924250ea4e530dbd0c2dbdd13857002a66f755b7
bryceco <bryceco@d6335498-75d6-11de-abb5-1d1887d8974b>
2010-03-25 05:54:30 +0000

    Add even more instrumentation to diagnose Issue #2

---
+++ nethack/win/cocoa/MainWindow.h
@@ -0,0 +1,16 @@
+//
+//  MainWindow.h
+//  NetHackCocoa
+//
+//  Created by Bryce on 3/24/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MainWindow : NSWindow {
+
+}
+
+@end
